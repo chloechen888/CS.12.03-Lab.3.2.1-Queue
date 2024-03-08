f1d84551ea685636e526d51685f6d4c267f975e8
chloechen <tszkc2024@student.cis.edu.hk>
2024-03-08 15:12:50 +0800

    First commit.

--- src/main/java/BreadthFirstSearch.java
+++ src/main/java/BreadthFirstSearch.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch {
 
@@ -7,37 +9,30 @@ public class BreadthFirstSearch {
      * @return - an array containing the order of nodes traversed via a Breadth First Search.
      */
     public static int[] bfs(int[][] graph) {
+        int numNodes = graph.length;
 
-        // Create a Queue and add the first node (0) to it. Use your Queue class that you just created!
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        boolean[] visited = new boolean[numNodes];
+        visited[0] = true;
+        int[] b = new int[numNodes];
+        int index = 0;
+        b[index++] = 0;
 
-        // Create a visited array. This array will track whether we have visited a specific node.
+        while (!queue.isEmpty()) {
 
-        // Create a bfs path array and a bfs path index. This array will track the order that the nodes were visited.
+            int currentNode = queue.poll();
+            int[] neighbors = graph[currentNode];
 
-        // Add the first node (0) to the queue.
-
-        // Record the first node (0) as visited.
-        
-        // Time to traverse the graph! While the queue is not empty ...
-
-            // Dequeue (poll) the queue and store this value in a variable called currentNode.
-
-            // Record this node as visited.
-
-            // Add this node to the path and update the path index.
-
-            // Obtain an array of this node's neighbouring/adjacent nodes.
-        
-            // Explore the current nodes neighbouring nodes. For each neighbouring node ...
-
-                // If a neighbour hasn't been visited before ...
-
-                    // Record the neighbour as visited.
-        
-                    // Add the neighbour to the queue. 
-
-        // Return bfs path.
-        
-        return null;
+            for (int n : neighbors) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                    b[index++] = n;
+                }
+            }
+        }
+        return b;
     }
+
 }
--- src/main/java/CISQueue.java
+++ src/main/java/CISQueue.java
@@ -1,28 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CISQueue<T> {
 
-    // A property of our Queue class is a Linked List.
-    // Use your Linked List class that you created in Lab 1!
-    // Thought 1: is aggregation or composition going to be used here?
-    // Thought 2: meditate on the word 'abstraction'.
+    private Queue<Integer> queue;
 
-    // Our CISQueue class will have a constructor.
-    // Our CISQueue constructor will instantiate a Queue of size 1.
+    public CISQueue(Queue<Integer> queue) {
+        this.queue = queue;
+    }
 
-    // isEmpty
-    // Returns a boolean.
-    // Indicates whether the Queue is empty.
+    public CISQueue(int i) {
+        this.queue = new LinkedList<>();
+        this.queue.add(i);
+    }
 
-    // size
-    // Returns an integer.
-    // Indicates how many elements are currently in our Queue.
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 
-    // dequeue
-    // Returns the data stored in the node that will be polled.
+    public int size() {
+        return queue.size();
+    }
 
-    // enqueue
-    // Adds a node to the end of our Queue.
+    public Integer dequeue() {
+        return queue.poll();
+    }
 
-    // toString
-    // Returns a String representation of our Queue.
+    public void enqueue(Integer data) {
+        queue.add(data);
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer i : queue) {
+            if (i == null) {
+                sb.append("null");
+            } else {
+                sb.append(i);
+            }
+            sb.append(" -> ");
+        }
+        sb.append("null");
+        return sb.toString();
+    }
 }
